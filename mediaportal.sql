48c40d74c9328b03c2a35604dad578953afa51e2
DrMonn <bjoernfelix.hell@tfobz.net>
2022-10-15 00:36:34 +0200

    Updated .sql file to match new users table structure

--- mediaportal.sql
+++ mediaportal.sql
@@ -1,96 +1,81 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
+-- MariaDB dump 10.19  Distrib 10.9.3-MariaDB, for Linux (x86_64)
 --
--- Host: 127.0.0.1
--- Erstellungszeit: 03. Okt 2022 um 14:22
--- Server-Version: 10.4.24-MariaDB
--- PHP-Version: 7.4.29
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: mediaportal
+-- ------------------------------------------------------
+-- Server version	10.9.3-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Datenbank: `mediaportal`
+-- Table structure for table `media`
 --
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `media`
---
-
+DROP TABLE IF EXISTS `media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `media` (
-  `mid` int(11) NOT NULL,
+  `mid` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL,
   `mtimestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `mmedia` longblob NOT NULL
+  `mmedia` longblob NOT NULL,
+  PRIMARY KEY (`mid`),
+  KEY `uid` (`uid`),
+  CONSTRAINT `media_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
-
--- --------------------------------------------------------
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Tabellenstruktur für Tabelle `users`
+-- Dumping data for table `media`
 --
 
+LOCK TABLES `media` WRITE;
+/*!40000 ALTER TABLE `media` DISABLE KEYS */;
+/*!40000 ALTER TABLE `media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `uid` int(11) NOT NULL,
-  `uusername` varchar(30) COLLATE latin1_german1_ci NOT NULL UNIQUE,
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `uusername` varchar(30) COLLATE latin1_german1_ci NOT NULL,
   `uemail` varchar(50) COLLATE latin1_german1_ci NOT NULL,
-  `upassword` varchar(100) COLLATE latin1_german1_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
+  `upassword` varchar(100) COLLATE latin1_german1_ci NOT NULL,
+  `ulogged` tinyint(4) DEFAULT 0,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `uusername` (`uusername`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1 COLLATE=latin1_german1_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indizes der exportierten Tabellen
+-- Dumping data for table `users`
 --
 
---
--- Indizes für die Tabelle `media`
---
-ALTER TABLE `media`
-  ADD PRIMARY KEY (`mid`),
-  ADD KEY `uid` (`uid`);
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indizes für die Tabelle `user`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`uid`);
-
---
--- AUTO_INCREMENT für exportierte Tabellen
---
-
---
--- AUTO_INCREMENT für Tabelle `media`
---
-ALTER TABLE `media`
-  MODIFY `mid` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT für Tabelle `user`
---
-ALTER TABLE `users`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints der exportierten Tabellen
---
-
---
--- Constraints der Tabelle `media`
---
-ALTER TABLE `media`
-  ADD CONSTRAINT `media_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-15  0:32:04
